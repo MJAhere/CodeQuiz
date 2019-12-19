29cc1617be703f7a9ea9554e14b08854de0dee9a
Michael Aiello <mjahere023@gmail.com>
2019-12-19 04:58:25 -0700

    Added html construct to page

--- script2.js
+++ script2.js
@@ -10,15 +10,19 @@ var questions = [
       answer: "parentheses"
     },
     {
-        Title: "Which data types are able to be used in an aaray?",
+        Title: "Which data types are able to be used in an array?",
         Choices: ["None", "strings,booleans and numbers", "numbers only", "strings only"],
         answer: "strings, booleans and numbers"
-    }
+    },
     {
-        Title: "How much wood could a wood chuck, chuck, if a wood chuck could chuck wood?"
-        Choices: ["no wood", "some wood", "wood chucks don't chuck", "All the wood"]
+        Title: "How much wood could a wood chuck, chuck, if a wood chuck could chuck wood?",
+        Choices: ["no wood", "some wood", "wood chucks don't chuck", "All the wood"],
         answer: "All the wood"
+    },
+    {
+      Title: "What is JQuery?",
+      Choices: ["All you can eat buffet.", "An illegal music site.", "A delicious type of cheese.", "A Javascript library that makes JS easier to use."],
+      answer: "A Javascript library that makes JS easier to use."
     }
-
   ];
   
