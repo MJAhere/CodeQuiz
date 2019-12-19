936398b8d0ac0766c75d8a67d6d707d91d06ef8e
Michael Aiello <mjahere023@gmail.com>
2019-12-18 20:26:29 -0700

    Added more questions to the array

--- script2.js
+++ script2.js
@@ -9,6 +9,16 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
-    ///etc.
+    {
+        Title: "Which data types are able to be used in an aaray?",
+        Choices: ["None", "strings,booleans and numbers", "numbers only", "strings only"],
+        answer: "strings, booleans and numbers"
+    }
+    {
+        Title: "How much wood could a wood chuck, chuck, if a wood chuck could chuck wood?"
+        Choices: ["no wood", "some wood", "wood chucks don't chuck", "All the wood"]
+        answer: "All the wood"
+    }
+
   ];
   
